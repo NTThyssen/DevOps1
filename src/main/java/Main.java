06e3ee8024d38cb40444c17ca11a7658199c6e23
futte111 <ntthyssen@gmail.com>
2019-09-03 10:16:25 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+//import org.apache.catalina.startup.Tomcat;
+//import org.apache.catalina.startup.Tomcat;
+//import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] ags){
+        Tomcat tomcat = new Tomcat();
+
+
+
+        tomcat = new Tomcat();
+        tomcat.setBaseDir("temp");
+        String port = System.getenv("PORT"); //Til Heroku
+        if (port==null){
+            port="8080";
+        }
+
+        tomcat.setPort(Integer.parseInt(port));
+        tomcat.getConnector();
+
+        tomcat.addWebapp("/", new File("src/main/webapp").getAbsolutePath());
+
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
+
+        tomcat.getServer().await();
+
+    }
+}
+
+
